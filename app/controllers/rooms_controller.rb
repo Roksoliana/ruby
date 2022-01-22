ecce5a5658e0ed42832199d1b25438c71c96b147
Roksoliana <yatsunda7@gmail.com>
2022-01-22 17:39:44 +0200

    Hide private rooms from rooms section

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,13 +1,15 @@
 class RoomsController < ApplicationController
+
   def index
     @rooms = Room.all
     @room = Room.new
-    @users = User.except_me(current_user)
+    @users = User.except_me(@current_user)
   end
 
   def show
-    @rooms = Room.all
-    @users = User.except_me(current_user)
+    @current_user = current_user
+    @rooms = Room.public_rooms
+    @users = User.except_me(@current_user)
     @room = Room.new
     @single_room = Room.find(params[:id])
     @message = Message.new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @rooms = Room.all
+    @rooms = Room.public_rooms
     @users = User.except_me(current_user)
     @room = Room.new
     @message = Message.new
