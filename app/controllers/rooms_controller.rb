1b086c10d333fb4980719fd957a9003d72cc91cf
Roksoliana <yatsunda7@gmail.com>
2022-01-23 00:19:07 +0200

    fix

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,13 +3,12 @@ class RoomsController < ApplicationController
   def index
     @rooms = Room.all
     @room = Room.new
-    @users = User.except_me(@current_user)
+    @users = User.except_me(current_user)
   end
 
   def show
-    @current_user = current_user
     @rooms = Room.public_rooms
-    @users = User.except_me(@current_user)
+    @users = User.except_me(current_user)
     @room = Room.new
     @single_room = Room.find(params[:id])
     @message = Message.new
